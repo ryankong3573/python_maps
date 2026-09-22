ae0943c873a5f2d796cf6a0b18597a53e5c0095c
ryankong3573 <ryankong3573@gmail.com>
2026-09-23 01:21:41 +0800

    implemented function for bfs

--- algorithms.py
+++ algorithms.py
@@ -1,10 +1,36 @@
 from dataclasses import dataclass
 from collections import deque
 
-def bfs_algorithm(grid, start, end):
+def get_neighbours(node):
+    pass
+
+def reconstruct_path(came_from,start,goal):
+    pass
+
+    
+
+def bfs_algorithm(grid, start, goal):
     frontier = deque()
     frontier.append(start)
     came_from = {start:None}
+    nodes_expanded = 0
+    visited = []
     
-    current = frontier.popleft()
+    while len(frontier) != 0:
+        current = frontier.popleft()
+
+        if current == goal:
+            break
+
+        for x in get_neighbours(current):
+            if x not in visited:
+                nodes_expanded += 1
+                came_from[x] = current
+                visited.append(x)
+                frontier.append(x)
+
+    path = reconstruct_path(came_from, start, goal)
+
+    return path, nodes_expanded
+
     
